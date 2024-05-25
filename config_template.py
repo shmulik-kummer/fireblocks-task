002814722e408237e2550a022e6945972ef98efb
Shmulik Kummer <kummersh@gmail.com>
2024-05-25 20:29:12 +0300

    Update template for gmail params

--- config_template.py
+++ config_template.py
@@ -9,6 +9,10 @@ API_KEY = 'YOUR_API_KEY'
 API_SECRET_PATH = 'path/to/your/fireblocks_secret.key'
 API_URL = 'https://api.fireblocks.io'
 
+# Gmail config
+EMAIL = "USER_EMAIL"
+EMAIL_APP_PASSWORD = 'GMAIL_APP_PASSWORD'
+
 # Load API secret (this line can be uncommented once you create your own config.py)
 # with open(API_SECRET_PATH, 'r') as file:
 #     API_SECRET = file.read()
